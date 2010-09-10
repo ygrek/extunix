dc3181c4e55b0bf8f24dbaf9fca7aa572e7f0868
Sylvain Le Gall <gildor@debian.org>
2010-09-10 08:32:05 +0000

    Inject an old fadvise binding

---
+++ src/TODO/fadvise/fadviseC.c
@@ -0,0 +1,92 @@
+
+/*
+ * Binding to posix_fadvise
+ *
+ * author: Sylvain Le Gall
+ *
+ */
+
+#include <caml/mlvalues.h>
+#include <caml/memory.h>
+
+#ifdef WINDOWS
+CAMLprim value caml_filescale_fadvise (value vfd, value voff, value vlen, value vadvise)
+{
+  CAMLparam0();
+  CAMLreturn(Val_unit);
+};
+
+CAMLprim value caml_filescale_fadvise64 (value vfd, value voff, value vlen, value vadvise)
+{
+  CAMLparam0();
+  CAMLreturn(Val_unit);
+};
+
+#else
+
+#define _XOPEN_SOURCE 600
+#define _LARGEFILE64_SOURCE 1
+
+#include <caml/alloc.h>
+#include <fcntl.h>
+
+static int caml_advises[] =
+  {
+    POSIX_FADV_NORMAL,
+    POSIX_FADV_SEQUENTIAL,
+    POSIX_FADV_RANDOM,
+    POSIX_FADV_NOREUSE,
+    POSIX_FADV_WILLNEED,
+    POSIX_FADV_DONTNEED
+  };
+
+CAMLprim value caml_filescale_fadvise64 (value vfd, value voff, value vlen, value vadvise)
+{
+  int     errcode = 0;
+  int     fd = -1;
+  off64_t off = 0;
+  off64_t len = 0;
+  int     advise = 0;
+
+  CAMLparam4(vfd, voff, vlen, vadvise);
+
+  fd  = Int_val(vfd);
+  off = Int64_val(voff);
+  len = Int64_val(vlen);
+  advise = caml_advises[Int_val(vadvise)]; 
+
+  errcode = posix_fadvise64(fd, off, len, advise);
+
+  if (errcode != 0)
+  {
+    unix_error(errcode, "fadvise", Nothing);
+  };
+
+  CAMLreturn(Val_unit);
+};
+
+CAMLprim value caml_filescale_fadvise (value vfd, value voff, value vlen, value vadvise)
+{
+  int   errcode = 0;
+  int   fd = -1;
+  off_t off = 0;
+  off_t len = 0;
+  int   advise = 0;
+
+  CAMLparam4(vfd, voff, vlen, vadvise);
+
+  fd  = Int_val(vfd);
+  off = Long_val(voff);
+  len = Long_val(vlen);
+  advise = caml_advises[Int_val(vadvise)]; 
+
+  errcode = posix_fadvise(fd, off, len, advise);
+
+  if (errcode != 0)
+  {
+    unix_error(errcode, "fadvise", Nothing);
+  };
+
+  CAMLreturn(Val_unit);
+};
+#endif
