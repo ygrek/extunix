7e52f688bd8f30f073905cbe43f16add1a0d2a74
ygrek <ygrek@autistici.org>
2011-02-14 17:19:47 +0200

    realpath: fix segfault on non-glibc systems

--- src/stdlib.c
+++ src/stdlib.c
@@ -4,6 +4,8 @@
 
 #if defined(EXTUNIX_HAVE_REALPATH)
 
+#ifdef __GLIBC__
+
 CAMLprim value caml_extunix_realpath(value v_path) 
 {
   CAMLparam1(v_path);
@@ -20,6 +22,31 @@ CAMLprim value caml_extunix_realpath(value v_path)
 
   CAMLreturn(v_s);
 }
+
+#else
+
+/* janest-core-0.6.0/lib/unix_ext_stubs.c */
+
+/* Seems like a sane approach to getting a reasonable bound for the
+   maximum path length */
+#ifdef PATH_MAX
+#define JANE_PATH_MAX ((PATH_MAX <= 0 || PATH_MAX > 65536) ? 65536 : PATH_MAX)
+#else
+#define JANE_PATH_MAX (65536)
+#endif
+
+CAMLprim value caml_extunix_realpath(value v_path)
+{
+  char *path = String_val(v_path);
+  /* [realpath] is inherently broken without GNU-extension, and this
+     seems like a reasonable thing to do if we do not build against
+     GLIBC. */
+  char resolved_path[JANE_PATH_MAX];
+  if (realpath(path, resolved_path) == NULL) uerror("realpath", v_path);
+  return caml_copy_string(resolved_path);
+}
+
+#endif /* __GLIBC__ */
 
 #endif
 
