9e748dbab59dc09921d1578fe761a776766b936f
Goswin von Brederlow <goswin-v-b@web.de>
2012-04-21 16:09:57 +0200

    Flesh out pread/pwrite modes for bigarray.

--- src/pread_pwrite_ba.c
+++ src/pread_pwrite_ba.c
@@ -3,6 +3,11 @@
 #define EXTUNIX_WANT_PWRITE
 #include "config.h"
 
+enum mode_bits { BIT_ONCE, BIT_NOERROR, BIT_NOINTR };
+#define ONCE (1 << BIT_ONCE)
+#define NOERROR (1 << BIT_NOERROR)
+#define NOINTR (1 << BIT_NOINTR)
+
 #if defined(EXTUNIX_HAVE_PREAD)
 
 /*  Copyright © 2012 Goswin von Brederlow <goswin-v-b@web.de>   */
@@ -10,7 +15,7 @@
 #include <errno.h>
 #include <string.h>
 
-CAMLprim value caml_extunixba_pread_common(value v_fd, off_t off, value v_buf, int once) {
+CAMLprim value caml_extunixba_pread_common(value v_fd, off_t off, value v_buf, int mode) {
     CAMLparam2(v_fd, v_buf);
     ssize_t ret;
     size_t fd = Int_val(v_fd);
@@ -24,41 +29,69 @@ CAMLprim value caml_extunixba_pread_common(value v_fd, off_t off, value v_buf, i
 	caml_leave_blocking_section();
 	if (ret == 0) break;
 	if (ret == -1) {
-	    if ((errno == EAGAIN || errno == EWOULDBLOCK) && processed > 0) break;
+	    if (errno == EINTR && (mode & NOINTR)) continue;
+	    if (processed > 0) {
+		if (errno == EAGAIN || errno == EWOULDBLOCK) break;
+		if (mode & NOERROR) break;
+	    }
 	    uerror("pread", Nothing);
 	}
 	processed += ret;
 	buf += ret;
 	off += ret;
 	len -= ret;
-	if (once) break;
+	if (mode & ONCE) break;
     }
 
     CAMLreturn(Val_long(processed));
 }
 
-value caml_extunixba_pread(value v_fd, value v_off, value v_buf)
+value caml_extunixba_all_pread(value v_fd, value v_off, value v_buf)
 {
     off_t off = Long_val(v_off);
-    return caml_extunixba_pread_common(v_fd, off, v_buf, 0);
+    return caml_extunixba_pread_common(v_fd, off, v_buf, NOINTR);
 }
 
 value caml_extunixba_single_pread(value v_fd, value v_off, value v_buf)
 {
     off_t off = Long_val(v_off);
-    return caml_extunixba_pread_common(v_fd, off, v_buf, 1);
+    return caml_extunixba_pread_common(v_fd, off, v_buf, ONCE);
 }
 
-value caml_extunixba_pread64(value v_fd, value v_off, value v_buf)
+value caml_extunixba_pread(value v_fd, value v_off, value v_buf)
+{
+    off_t off = Long_val(v_off);
+    return caml_extunixba_pread_common(v_fd, off, v_buf, NOINTR | NOERROR);
+}
+
+value caml_extunixba_intr_pread(value v_fd, value v_off, value v_buf)
+{
+    off_t off = Long_val(v_off);
+    return caml_extunixba_pread_common(v_fd, off, v_buf, NOERROR);
+}
+
+value caml_extunixba_all_pread64(value v_fd, value v_off, value v_buf)
 {
     off_t off = Int64_val(v_off);
-    return caml_extunixba_pread_common(v_fd, off, v_buf, 0);
+    return caml_extunixba_pread_common(v_fd, off, v_buf, NOINTR);
 }
 
 value caml_extunixba_single_pread64(value v_fd, value v_off, value v_buf)
 {
     off_t off = Int64_val(v_off);
-    return caml_extunixba_pread_common(v_fd, off, v_buf, 1);
+    return caml_extunixba_pread_common(v_fd, off, v_buf, ONCE);
+}
+
+value caml_extunixba_pread64(value v_fd, value v_off, value v_buf)
+{
+    off_t off = Int64_val(v_off);
+    return caml_extunixba_pread_common(v_fd, off, v_buf, NOINTR | NOERROR);
+}
+
+value caml_extunixba_intr_pread64(value v_fd, value v_off, value v_buf)
+{
+    off_t off = Int64_val(v_off);
+    return caml_extunixba_pread_common(v_fd, off, v_buf, NOERROR);
 }
 #endif
 
@@ -68,7 +101,7 @@ value caml_extunixba_single_pread64(value v_fd, value v_off, value v_buf)
 
 #include <string.h>
 
-CAMLprim value caml_extunixba_pwrite_common(value v_fd, off_t off, value v_buf, int once) {
+CAMLprim value caml_extunixba_pwrite_common(value v_fd, off_t off, value v_buf, int mode) {
     CAMLparam2(v_fd, v_buf);
     ssize_t ret;
     size_t fd = Int_val(v_fd);
@@ -82,41 +115,69 @@ CAMLprim value caml_extunixba_pwrite_common(value v_fd, off_t off, value v_buf, 
 	caml_leave_blocking_section();
 	if (ret == 0) break;
 	if (ret == -1) {
-	    if ((errno == EAGAIN || errno == EWOULDBLOCK) && processed > 0) break;
+	    if (errno == EINTR && (mode & NOINTR)) continue;
+	    if (processed > 0){
+		if (errno == EAGAIN || errno == EWOULDBLOCK) break;
+		if (mode & NOERROR) break;
+	    }
 	    uerror("pwrite", Nothing);
 	}
 	processed += ret;
 	buf += ret;
 	off += ret;
 	len -= ret;
-	if (once) break;
+	if (mode & ONCE) break;
     }
 
     CAMLreturn(Val_long(processed));
 }
 
-value caml_extunixba_pwrite(value v_fd, value v_off, value v_buf)
+value caml_extunixba_all_pwrite(value v_fd, value v_off, value v_buf)
 {
     off_t off = Long_val(v_off);
-    return caml_extunixba_pwrite_common(v_fd, off, v_buf, 0);
+    return caml_extunixba_pwrite_common(v_fd, off, v_buf, NOINTR);
 }
 
 value caml_extunixba_single_pwrite(value v_fd, value v_off, value v_buf)
 {
     off_t off = Long_val(v_off);
-    return caml_extunixba_pwrite_common(v_fd, off, v_buf, 1);
+    return caml_extunixba_pwrite_common(v_fd, off, v_buf, ONCE);
 }
 
-value caml_extunixba_pwrite64(value v_fd, value v_off, value v_buf)
+value caml_extunixba_pwrite(value v_fd, value v_off, value v_buf)
+{
+    off_t off = Long_val(v_off);
+    return caml_extunixba_pwrite_common(v_fd, off, v_buf, NOINTR | NOERROR);
+}
+
+value caml_extunixba_intr_pwrite(value v_fd, value v_off, value v_buf)
+{
+    off_t off = Long_val(v_off);
+    return caml_extunixba_pwrite_common(v_fd, off, v_buf, NOERROR);
+}
+
+value caml_extunixba_all_pwrite64(value v_fd, value v_off, value v_buf)
 {
     off_t off = Int64_val(v_off);
-    return caml_extunixba_pwrite_common(v_fd, off, v_buf, 0);
+    return caml_extunixba_pwrite_common(v_fd, off, v_buf, NOINTR);
 }
 
 value caml_extunixba_single_pwrite64(value v_fd, value v_off, value v_buf)
 {
     off_t off = Int64_val(v_off);
-    return caml_extunixba_pwrite_common(v_fd, off, v_buf, 1);
+    return caml_extunixba_pwrite_common(v_fd, off, v_buf, ONCE);
+}
+
+value caml_extunixba_pwrite64(value v_fd, value v_off, value v_buf)
+{
+    off_t off = Int64_val(v_off);
+    return caml_extunixba_pwrite_common(v_fd, off, v_buf, NOINTR | NOERROR);
+}
+
+value caml_extunixba_intr_pwrite64(value v_fd, value v_off, value v_buf)
+{
+    off_t off = Int64_val(v_off);
+    return caml_extunixba_pwrite_common(v_fd, off, v_buf, NOERROR);
 }
 #endif
 
