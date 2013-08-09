c5e496ae7e7ca61021b19d1de2f43dcda3c1747d
ygrek <ygrek@autistici.org>
2013-08-09 17:18:08 +0800

    OCaml 4.01 compatibility (Unix.open_flag)

--- src/common.c
+++ src/common.c
@@ -16,10 +16,13 @@
 #ifndef O_RSYNC
 #define O_RSYNC 0
 #endif
+#ifndef O_CLOEXEC
+#define O_CLOEXEC 0
+#endif
 
 static int open_flag_table[] = {
   O_RDONLY, O_WRONLY, O_RDWR, O_NONBLOCK, O_APPEND, O_CREAT, O_TRUNC, O_EXCL,
-  O_NOCTTY, O_DSYNC, O_SYNC, O_RSYNC, 0 /* O_SHARE_DELETE */
+  O_NOCTTY, O_DSYNC, O_SYNC, O_RSYNC, 0 /* O_SHARE_DELETE */, O_CLOEXEC,
 };
 
 int extunix_open_flags(value v_flags)
