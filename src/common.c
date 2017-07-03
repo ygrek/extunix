1f2cf0e1540ddddccd2718f8d63879943d2cb6cc
ygrek <ygrek@autistici.org>
2017-07-02 21:08:21 -0700

    OCaml 4.05 compatibility (O_KEEPEXEC)

--- src/common.c
+++ src/common.c
@@ -26,10 +26,13 @@
 #ifndef O_CLOEXEC
 #define O_CLOEXEC 0
 #endif
+#ifndef O_KEEPEXEC
+#define O_KEEPEXEC 0
+#endif
 
 static int open_flag_table[] = {
   O_RDONLY, O_WRONLY, O_RDWR, O_NONBLOCK, O_APPEND, O_CREAT, O_TRUNC, O_EXCL,
-  O_NOCTTY, O_DSYNC, O_SYNC, O_RSYNC, 0 /* O_SHARE_DELETE */, O_CLOEXEC,
+  O_NOCTTY, O_DSYNC, O_SYNC, O_RSYNC, 0 /* O_SHARE_DELETE */, O_CLOEXEC, O_KEEPEXEC,
 };
 
 int extunix_open_flags(value list)
