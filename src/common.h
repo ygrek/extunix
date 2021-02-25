05f2e90a5b5c0c8007d2eae9b40a83a7305bdb76
Antonin Décimo <antonin@tarides.com>
2021-02-25 13:22:51 +0100

    Don't redefine option macros with OCaml >= 4.12

--- src/common.h
+++ src/common.h
@@ -1,7 +1,9 @@
-
 #define UNUSED(x) (void)(x)
 
+#include <caml/version.h>
+#if OCAML_VERSION < 41200
 #define Val_none Val_int(0)
 #define Some_val(v) Field(v,0)
+#endif
 
 int extunix_open_flags(value);
