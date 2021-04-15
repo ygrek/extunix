4783d7f2f98b25bd1a26397fc37125ce0b4e1854
Antonin Décimo <antonin@tarides.com>
2021-04-15 12:08:21 +0200

    Constify string and add CAMLparam1 macro to non-glibc realpath stub

--- src/stdlib.c
+++ src/stdlib.c
@@ -40,7 +40,8 @@ CAMLprim value caml_extunix_realpath(value v_path)
 
 CAMLprim value caml_extunix_realpath(value v_path)
 {
-  char *path = String_val(v_path);
+  CAMLparam1(v_path);
+  const char *path = String_val(v_path);
   /* [realpath] is inherently broken without GNU-extension, and this
      seems like a reasonable thing to do if we do not build against
      GLIBC. */
