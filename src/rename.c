fdc25bf6439c1baa40330f38ac298cd70f034a71
Antonin Décimo <antonin@tarides.com>
2025-03-11 15:16:22 +0100

    Remove incorrect const qualifier

--- src/rename.c
+++ src/rename.c
@@ -31,8 +31,8 @@ CAMLprim value caml_extunix_renameat2(value v_oldfd, value v_oldname, value v_ne
   CAMLparam5(v_oldfd, v_oldname, v_newfd, v_newname, v_flags);
   check_flag_list(v_flags);
   int oldfd = Int_val(v_oldfd), newfd = Int_val(v_newfd);
-  const char *oldname = caml_stat_strdup(String_val(v_oldname)),
-             *newname = caml_stat_strdup(String_val(v_newname));
+  char *oldname = caml_stat_strdup(String_val(v_oldname)),
+       *newname = caml_stat_strdup(String_val(v_newname));
   int flags = caml_convert_flag_list(v_flags, rename_flags_table);
   caml_enter_blocking_section();
   int ret = renameat2(oldfd, oldname, newfd, newname, flags);
